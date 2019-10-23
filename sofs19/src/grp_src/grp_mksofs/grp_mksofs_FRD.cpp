04041682ddd89e6ee0ddad3b858935caf4086d14
eduardocoelho11 <eduardoluis1337@gmail.com>
2019-10-23 01:54:01 +0100

    mksofs - FillRootDirectory

--- sofs19/src/grp_src/grp_mksofs/grp_mksofs_FRD.cpp
+++ sofs19/src/grp_src/grp_mksofs/grp_mksofs_FRD.cpp
@@ -1,11 +1,12 @@
-#include "grp_mksofs.h"
+#include "../../../include/grp/grp_mksofs.h"
 
-#include "rawdisk.h"
-#include "core.h"
-#include "bin_mksofs.h"
+#include "../../../include/rawdisk.h"
+#include "../../../include/core.h"
+#include "../../../include/bin/bin_mksofs.h"
 
 #include <string.h>
 #include <inttypes.h>
+
 
 namespace sofs19
 {
@@ -13,14 +14,31 @@ namespace sofs19
        filling in the contents of the root directory:
        the first 2 entries are filled in with "." and ".." references
        the other entries are empty.
-       If rdsize is 2, a second block exists and should be filled as well.
        */
     void grpFillRootDir(uint32_t itotal)
     {
         soProbe(606, "%s(%u)\n", __FUNCTION__, itotal);
 
         /* change the following line by your code */
-        return binFillRootDir(itotal);
+        //return binFillRootDir(itotal);
+
+        SODirEntry direntry[DPB];            //SODirEntry buffer
+
+        int firstblock = 1 + itotal/8;
+
+        memset(direntry,0,BlockSize);              //field name filled with zeros
+
+        for(uint32_t i=2; i<DPB; i++) {      //the other slots must be cleaned: field inode filled with NullReference
+            direntry[i].in = NullReference;
+        }
+
+        memcpy(direntry[0].name, ".", 2);       //the first two entries are filled in with "." and ".." directory entries
+        memcpy(direntry[1].name, "..", 2); 
+
+        soWriteRawBlock(firstblock,direntry);            //[in] n   -> physical number of the block to be written into
+                                                //[in] buf -> pointer to the buffer containing the data to be written from 
+                                                //formata o bloco com os valores do dir
+        
     }
 };
 
