654e235ff2754612bc3e16d07035d17278c6166c
SamuelRDuarte <samuelduarte10@ua.pt>
2019-11-17 13:00:56 +0000

    Traverse_Path feito

--- sofs19/src/grp_src/grp_direntries/grp_traverse_path.cpp
+++ sofs19/src/grp_src/grp_direntries/grp_traverse_path.cpp
@@ -18,9 +18,35 @@ namespace sofs19
     uint32_t grpTraversePath(char *path)
     {
         soProbe(221, "%s(%s)\n", __FUNCTION__, path);
+        
+        if(strcmp(path, "/") == 0)
+            return 0;
 
-        /* change the following line by your code */
-        return binTraversePath(path);
+        if(path[0] != '/')
+            throw SOException(EINVAL, __FUNCTION__);
+
+        char *baseName = basename(strdupa(path));
+        char *dirName = dirname(strdupa(path));
+    
+        uint32_t inode = grpTraversePath(dirName);
+
+        uint32_t ih = soOpenInode(inode);
+        SOInode* ip = soGetInodePointer(ih);
+
+        if((ip->mode & S_IFDIR) != S_IFDIR)
+            throw SOException(ENOTDIR, __FUNCTION__);
+
+        if(!soCheckInodeAccess(ih, X_OK))
+            throw SOException(EACCES, __FUNCTION__);
+        
+        uint32_t in = soGetDirEntry(ih,baseName);
+
+        if(in == NullReference)
+            throw SOException(ENOENT, __FUNCTION__);
+
+        soCloseInode(ih);
+        return in;
+        
     }
 };
 
