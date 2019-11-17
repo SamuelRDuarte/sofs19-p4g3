cc0d0c45a25458c78dab3268b2c4658b6329778a
Alexandre Abreu <alexandre.abreu@ua.pt>
2019-11-17 19:48:47 +0000

    FillReferenceDataBlocks

--- sofs19/src/grp_src/grp_mksofs/grp_mksofs_FRDB.cpp
+++ sofs19/src/grp_src/grp_mksofs/grp_mksofs_FRDB.cpp
@@ -14,7 +14,33 @@ namespace sofs19
         soProbe(605, "%s(%u, %u, %u)\n", __FUNCTION__, ntotal, itotal, nbref);
 
         /* change the following line by your code */
-        return binFillReferenceDataBlocks(ntotal, itotal, nbref);
+        //return binFillReferenceDataBlocks(ntotal, itotal, nbref);
+        uint32_t nDataBlocks = ntotal - 1 - itotal/IPB - nbref; //número de blocos vazios
+        if(nDataBlocks > 64) {
+            uint32_t count = 1 + nbref + 64;
+
+            for(int i=1; i<=nbref; i++){
+                uint32_t block[RPB];
+                if(i==nbref){
+                    block[0] = NullReference;
+                }
+                else {
+                    block[0] = i+1;
+                }   
+                for(int j = 1; j <= RPB; j++){
+                    if(count > nDataBlocks + 1){
+                        block[j] = NullReference;
+                    }
+                    else {
+                        block[j] = count;
+                        count++;
+                    }
+                }
+                count--;
+                soWriteRawBlock(i + 1 + itotal/IPB, &block);
+                
+            }
+        }
     }
-};
+}
 
